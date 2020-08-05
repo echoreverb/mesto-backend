0dcc029650b4ad6794f37423f18b9e118990eb06
Alexey Dikarev <mailsub@yandex.ru>
2020-08-05 19:49:09 +0300

    fix: validate cardId, userId as hexes

--- routes/cards.js
+++ routes/cards.js
@@ -19,19 +19,19 @@ router.get('/', getCards);
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().required().hex(),
   }),
 }), deleteCard);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().required().hex(),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().required().hex(),
   }),
 }), dislikeCard);
 
